d09a6e3b202aa24eec9e59de924f07153569eb06
CarlosAmorim <carlos.av.amorim@gmail.com>
2022-10-29 16:40:00 -0300

    Style: Created Banner component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <div class="main-container">
-      <h1>Monte o seu burger:</h1>
-    </div>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu burger:</h1>
+    <p>Formulário</p>
   </div>
 </template>
+
+<script>
+import Banner from "@/components/Banner.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Banner,
+  },
+};
+</script>

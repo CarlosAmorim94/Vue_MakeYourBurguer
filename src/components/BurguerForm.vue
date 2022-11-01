2f8ef830f1f8b28bbacd2c1d50370fdf6cb7cb65
CarlosAmorim <carlos.av.amorim@gmail.com>
2022-11-01 10:45:23 -0300

    Feat: Add Message after burguer request

--- src/components/BurguerForm.vue
+++ src/components/BurguerForm.vue
@@ -56,13 +56,14 @@
 </template>
 
 <script>
+import Message from "./Message";
 export default {
-  name: "BurguerForm",
+  name: "BurgerForm",
   data() {
     return {
       paes: null,
       carnes: null,
-      opicionaisdata: null,
+      opcionaisdata: null,
       nome: null,
       pao: null,
       carne: null,
@@ -79,9 +80,38 @@ export default {
       this.carnes = data.carnes;
       this.opcionaisdata = data.opcionais;
     },
+    async createBurger(e) {
+      e.preventDefault();
+      const data = {
+        nome: this.nome,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado",
+      };
+      const dataJson = JSON.stringify(data);
+      const req = await fetch("http://localhost:3000/burgers", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson,
+      });
+      const res = await req.json();
+      console.log(res);
+      this.msg = `Pedido Nº ${res.id} realizado com sucesso!`;
+      // clear message
+      setTimeout(() => (this.msg = ""), 3000);
+      // limpar campos
+      this.nome = "";
+      this.carne = "";
+      this.pao = "";
+      this.opcionais = [];
+    },
   },
   mounted() {
     this.getIngredientes();
+  },
+  components: {
+    Message,
   },
 };
 </script>
